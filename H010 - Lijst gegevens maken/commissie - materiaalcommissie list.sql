88725ccc3755c5e246ce7c082a27bf049f8b66fa
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-02-02 13:49:06 +0100

    H010

---
+++ H010 - Lijst gegevens maken/commissie - materiaalcommissie list.sql
@@ -0,0 +1,14 @@
+#CREATE TABLE materiaalcommissie (
+#	naam varchar(255),
+#    telefoonnummer varchar(255)
+#);
+
+#INSERT INTO materiaalcommissie (naam, telefoonnummer) VALUES ("J. Jansen", "036-3568912"),
+#															  ("F. van Bosch", "06-98653214"),
+#                                                             ("M. Mathijsen", "0320-827364"),
+#                                                             ("M. H. Abdelkader", "06-19283765"),
+#                                                             ("T. Mobile", "06-61278394"),
+#                                                             ("W. uit de Woning", "0320-445566");
+
+#SELECT naam, telefoonnummer FROM materiaalcommissie;      
+SELECT COUNT(*) FROM materiaalcommissie;                                                       
